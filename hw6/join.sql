017eb5df221042ef6910790f000ad6ccc4ecd09d
Jon <jon.hogan83@gmail.com>
2022-11-21 10:54:26 -0600

    Completed join query

--- hw6/join.sql
+++ hw6/join.sql
@@ -1,5 +1,3 @@
-SELECT Customer_T.CustomerID, CustomerName, Order_T.OrderID, OrderDate, OrderedQuanity as TotalQuantity
+SELECT Customer_T.CustomerID, CustomerName, Order_T.OrderID, OrderDate, SUM(OrderedQuantity) AS TotalQuantity
 FROM Customer_T , Order_T, OrderLine_T
-Join Order_T on Customer_T.CustomerID = Order_T.CustomerID
-Join OrderLine_T on Order_T.OrderID = OrderLine_T.OrderID
-WHERE Customer_T.CustomerID = Order_T.CustomerID;
+WHERE Order_T.CustomerID = Customer_T.CustomerID and OrderLine_T.OrderID = Order_T.OrderID AND Customer_T.CustomerID = 1 GROUP BY OrderID;
